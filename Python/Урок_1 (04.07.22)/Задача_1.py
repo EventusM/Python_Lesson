0ea0b041a0e4dfabbb345188707314c4c32ffe8e
Maxim <uly.maxim94@mail.ru>
2022-07-06 22:32:37 +0300

    с переносом строк в коде

--- Python/Урок_1 (04.07.22)/Задача_1.py
+++ Python/Урок_1 (04.07.22)/Задача_1.py
@@ -1,4 +1,7 @@
 name = str(input("Имя "))
 date_birthday = int(input("Введите год рождения: "))
 day = int(input("Введите текущий год: "))
-print("Вы ввели: " "\n Имя: ", name, "\n Год рождения: ", date_birthday, "\n Ваш возраст: ", day - date_birthday)
+print("Вы ввели: ",
+      "\n Имя: ", name,
+      "\n Год рождения: ", date_birthday,
+      "\n Ваш возраст: ", day - date_birthday)
