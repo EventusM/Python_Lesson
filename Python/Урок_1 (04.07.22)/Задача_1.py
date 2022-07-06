e3635002f4fd1a47153d3993f04d4bf8e70805b2
Maxim <uly.maxim94@mail.ru>
2022-07-06 22:17:02 +0300

    выполнено

--- Python/Урок_1 (04.07.22)/Задача_1.py
+++ Python/Урок_1 (04.07.22)/Задача_1.py
@@ -1,1 +1,5 @@
-привет
+
+name = str(input("Имя "))
+date_birthday = int(input("Введите год рождения: "))
+day = int(input("Введите текущий год: "))
+print("Вы ввели: " "\n Имя: ", name, "\n Год рождения: ", date_birthday, "\n Ваш возраст: ", day - date_birthday)
