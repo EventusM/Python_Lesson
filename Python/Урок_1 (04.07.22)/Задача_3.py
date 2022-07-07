5faf0f7d1412d77c6b9b346446c09076de7d4b01
Maxim <uly.maxim94@mail.ru>
2022-07-08 00:17:13 +0300

    1.0 с доп переменной

--- Python/Урок_1 (04.07.22)/Задача_3.py
+++ Python/Урок_1 (04.07.22)/Задача_3.py
@@ -1,1 +1,10 @@
-привет
+n = int(input('введите число: '))
+number = str(n)
+num_1 = number + number
+num_2 = number + number + number
+sum = n + int(num_1) + int(num_2)
+print(f'{n} + {num_1} + {num_2} = {sum}')
+
+
+
+
