7ef8d9f633b6c96c1add82ff6e83ec9bc60e0ae4
Maxim <uly.maxim94@mail.ru>
2022-07-08 00:25:34 +0300

    1.1 с доп переменной

--- Python/Урок_1 (04.07.22)/Задача_3.py
+++ Python/Урок_1 (04.07.22)/Задача_3.py
@@ -1,8 +1,8 @@
 n = int(input('введите число: '))
-number = str(n)
-num_1 = number + number
-num_2 = number + number + number
-sum = n + int(num_1) + int(num_2)
+n_fake = str(n)
+num_1 = n_fake + n_fake
+num_2 = n_fake + n_fake + n_fake
+sum = int(n) + int(num_1) + int(num_2)
 print(f'{n} + {num_1} + {num_2} = {sum}')
 
 
