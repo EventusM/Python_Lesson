e9834bd2c01364524edd276891ed1149184c4699
Maxim <uly.maxim94@mail.ru>
2022-07-07 23:08:03 +0300

    1.0

--- Python/Урок_1 (04.07.22)/Задача_2.py
+++ Python/Урок_1 (04.07.22)/Задача_2.py
@@ -1,1 +1,6 @@
-привет
+sekond_o = int(input('Bведите секунды: '))
+minutes_sum = sekond_o // 60
+sekonds = sekond_o - (minutes_sum * 60)
+hour = minutes_sum // 60
+minutes = minutes_sum - (hour * 60)
+print(f'{hour:02}: {minutes:02}: {sekonds:02}')
