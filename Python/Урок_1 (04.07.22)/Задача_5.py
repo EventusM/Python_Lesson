72161ea1b14159c1a78c717315c716e6dc02ed88
Maxim <uly.maxim94@mail.ru>
2022-07-09 22:09:35 +0300

    в 6 задаче с выводом результата в консоль, согласно примеру из задачи.

--- Python/Урок_1 (04.07.22)/Задача_5.py
+++ Python/Урок_1 (04.07.22)/Задача_5.py
@@ -1,1 +1,15 @@
-привет
+profit = float(input('Какая выручка?: '))
+lesion = float(input('Сколько издержек?: '))
+net_profit = profit - lesion
+if net_profit > 0:
+    print(f'Чистая прибыль = {net_profit}руб.\nПоложительный финансовый результат :) \n')
+    employee = int(input('Количество сотрудников?: '))
+    print(f'Прибыль на одного сотрудника = {net_profit / employee:.2f}руб.\n'
+          f'Рентабельность компании = {(net_profit / lesion) * 100:.1f}%.')
+elif net_profit < 0:
+    print(f'Чистая прибыль = {net_profit}руб.\n'
+          f'Отрицательный финансовый результат :(\n')
+else:
+    print(f'Чистая прибыль = {net_profit}руб.\n'
+          f'Отсутствует прибыль :| \n')
+
--- Python/Урок_1 (04.07.22)/Задача_6.py
+++ Python/Урок_1 (04.07.22)/Задача_6.py
@@ -1,1 +1,20 @@
-привет
+while True:
+    km_1 = float(input('Сколько километров спортсмен пробежал в первый день? '))
+    finish = float(input('Желаемый результат? '))
+    if km_1 <= 0 or finish <= 0 or km_1 > finish:
+        print('Результата не получится.')
+        break
+    km_sum = 0
+    day_1 = 1
+    while True:
+        print(f'{day_1}-й день {km_1:.02f} км.')
+        if km_1 <= finish:
+            km_sum = km_1 * 0.1
+            km_1 = km_1 + km_sum
+            day_1 += 1
+        else:
+            print(f'На {day_1}-й день спортсмен достигнет результата - около {km_1:.0f}.км.')
+            break
+
+
+
